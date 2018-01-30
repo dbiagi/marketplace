cf212e95953d726632b5743558f73352492eafb8
Diego de Biagi <diegobiagiviana@gmail.com>
2018-01-29 23:39:56 -0200

    Added validation error response class.

--- src/main/java/org/dbiagi/marketplace/core/response/ValidationErrorResponse.java
+++ src/main/java/org/dbiagi/marketplace/core/response/ValidationErrorResponse.java
@@ -2,8 +2,17 @@ package org.dbiagi.marketplace.core.response;
 
 import org.dbiagi.marketplace.core.validation.ValidationError;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ValidationErrorResponse {
-    private List<ValidationError> validationErrorList;
+    private List<ValidationError> errors = new ArrayList<>();
+
+    public void addValidationError(ValidationError validationError) {
+        errors.add(validationError);
+    }
+
+    public List<ValidationError> getErrors() {
+        return errors;
+    }
 }
---
+++ src/test/java/org/dbiagi/marketplace/core/response/ValidationErrorResponseTest.java
@@ -0,0 +1,28 @@
+package org.dbiagi.marketplace.core.response;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.dbiagi.marketplace.core.validation.ValidationError;
+import org.junit.Assert;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ValidationErrorResponseTest {
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Test
+    public void testSerialization() throws JsonProcessingException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        ValidationErrorResponse response = new ValidationErrorResponse();
+
+        for (int i = 0; i < 10; i++) {
+            response.addValidationError(new ValidationError("00" + i, "Validation Error " + i));
+        }
+
+        String result = objectMapper.writeValueAsString(response);
+        logger.debug(result);
+
+        Assert.assertTrue(result.length() > 0);
+    }
+}
