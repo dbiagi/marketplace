f3cf3069cc4c0aee481059fea465a366ebe510cc
Diego de Biagi <diegobiagiviana@gmail.com>
2018-06-07 00:03:24 -0300

    Added beansvalidation lib to classification module.

--- classification/src/main/java/org/dbiagi/classification/entity/Category.java
+++ classification/src/main/java/org/dbiagi/classification/entity/Category.java
@@ -6,6 +6,10 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
 
 @Entity
 public class Category extends BaseEntity implements CategoryInterface {
@@ -13,9 +17,15 @@ public class Category extends BaseEntity implements CategoryInterface {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotEmpty
     private String name;
 
+    @NotEmpty
     private String slug;
+
+    private Category parent;
+
+    private List<Category> children;
 
     @Override
     public int getId() {
@@ -40,5 +50,35 @@ public class Category extends BaseEntity implements CategoryInterface {
     @Override
     public void setSlug(String slug) {
         this.slug = slug;
+    }
+
+    @Override
+    public Category getParent() {
+        return parent;
+    }
+
+    @Override
+    public void setParent(CategoryInterface parent) {
+        this.parent = (Category) parent;
+    }
+
+    @Override
+    public Collection<? extends CategoryInterface> getChildren() {
+        return children;
+    }
+
+    @Override
+    public void setChildren(Collection<? extends CategoryInterface> children) {
+        this.children = new ArrayList<>();
+        children.forEach(c -> this.children.add((Category) c));
+    }
+
+    @Override
+    public void addChild(CategoryInterface child) {
+        if (child instanceof Category && !children.contains(child)) {
+            child.setParent(this);
+
+            children.add((Category) child);
+        }
     }
 }
--- classification/src/main/java/org/dbiagi/classification/model/CategoryInterface.java
+++ classification/src/main/java/org/dbiagi/classification/model/CategoryInterface.java
@@ -1,5 +1,7 @@
 package org.dbiagi.classification.model;
 
+import java.util.Collection;
+
 public interface CategoryInterface extends BaseModel {
     String getName();
 
@@ -8,4 +10,14 @@ public interface CategoryInterface extends BaseModel {
     String getSlug();
 
     void setSlug(String slug);
+
+    CategoryInterface getParent();
+
+    void setParent(CategoryInterface parent);
+
+    Collection<? extends CategoryInterface> getChildren();
+
+    void setChildren(Collection<? extends CategoryInterface> children);
+
+    void addChild(CategoryInterface child);
 }
