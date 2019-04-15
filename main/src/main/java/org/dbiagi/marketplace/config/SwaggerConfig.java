8337c3f46bd25f9037c937d3d2871b7fc61775b8
Diego de Biagi <dbiagi@esapiens.com.br>
2019-04-15 18:50:23 -0300

    Removed broken import from springfox data configuration.

--- main/src/main/java/org/dbiagi/marketplace/config/SwaggerConfig.java
+++ main/src/main/java/org/dbiagi/marketplace/config/SwaggerConfig.java
@@ -3,14 +3,12 @@ package org.dbiagi.marketplace.config;
 import org.dbiagi.marketplace.Application;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -20,7 +18,6 @@ import static springfox.documentation.builders.PathSelectors.any;
 
 @EnableSwagger2
 @Configuration
-@Import({SpringDataRestConfiguration.class})
 public class SwaggerConfig extends WebMvcConfigurationSupport {
     private ApiInfo getMetadata() {
         return new ApiInfoBuilder()
